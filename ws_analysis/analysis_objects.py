c1b366b95e676f49bf2040e7cc25f58b50129866
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-21 15:35:33 +0100

    mod: changed var naming convention in user_correlations, using arryIndepVarObjects_dict to better match WSiOS

--- ws_analysis/analysis_objects.py
+++ ws_analysis/analysis_objects.py
@@ -8,24 +8,24 @@ from .analysis_heart_rate import create_df_daily_heart_rate
 def user_correlations(user_id):
     print("*** in user_correlations ***")
     df, list_of_user_data = create_user_df(user_id=user_id)
-    list_of_correlations_dict = []
+    list_of_arryIndepVarObjects_dict = []
     if 'HKCategoryTypeIdentifierSleepAnalysis' in list_of_user_data:
-        correlations_dict = {}
+        arryIndepVarObjects_dict = {}
         # corr_sleep_steps(df)
-        correlations_dict["name"]= "Step Count"
-        correlations_dict["depVarName"]= "Sleep Time"
-        correlations_dict["correlationValue"]= corr_sleep_steps(df)
-        list_of_correlations_dict.append(correlations_dict)
+        arryIndepVarObjects_dict["name"]= "Step Count"
+        arryIndepVarObjects_dict["depVarName"]= "Sleep Time"
+        arryIndepVarObjects_dict["correlationValue"]= corr_sleep_steps(df)
+        list_of_arryIndepVarObjects_dict.append(correlations_dict)
 
     if 'HKQuantityTypeIdentifierHeartRate' in list_of_user_data:
-        correlations_dict = {}
+        arryIndepVarObjects_dict = {}
         # corr_sleep_heart_rate(df)
-        correlations_dict["name"]= "Heart Rate Avg"
-        correlations_dict["depVarName"]= "Sleep Time"
-        correlations_dict["correlationValue"]= corr_sleep_heart_rate(df)
-        list_of_correlations_dict.append(correlations_dict)
+        arryIndepVarObjects_dict["name"]= "Heart Rate Avg"
+        arryIndepVarObjects_dict["depVarName"]= "Sleep Time"
+        arryIndepVarObjects_dict["correlationValue"]= corr_sleep_heart_rate(df)
+        list_of_arryIndepVarObjects_dict.append(correlations_dict)
     
-    return list_of_correlations_dict
+    return list_of_arryIndepVarObjects_dict
 
 def corr_sleep_steps(df):
     print("*** in corr_sleep_steps ***")
@@ -44,7 +44,7 @@ def corr_sleep_steps(df):
 
             # Calculate the correlation between step_count and sleepTimeFr
             correlation = df_daily_sleep_steps['step_count'].corr(df_daily_sleep_steps['sleepTimeFr'])
-            print(f"correlation: {correlation}, corr type: {type(correlation)}")
+            print(f"correlation: {correlation}, corr type: {correlation}")
             return correlation
         else:
             return "insufficient data"
@@ -62,13 +62,13 @@ def corr_sleep_heart_rate(df):
     df_daily_heart_rate = create_df_daily_heart_rate(df)
     try:
         if len(df_daily_heart_rate) > 5:# arbitrary minimum
-            
+
             # This will keep only the rows that have matching 'dateFr' values in both dataframes
             df_daily_sleep_steps = pd.merge(df_daily_sleep,df_daily_steps, on='dateFr')
 
             # Calculate the correlation between step_count and sleepTimeFr
             correlation = df_daily_sleep_steps['step_count'].corr(df_daily_sleep_steps['sleepTimeFr'])
-            print(f"correlation: {correlation}, corr type: {type(correlation)}")
+            print(f"correlation: {correlation}, corr type: {correlation}")
             return correlation
         else:
             return "insufficient data"
