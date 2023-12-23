8aabf3980f12a4cd07d832f1868a2484847219ba
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-23 22:51:15 +0100

    mod: restructured analysis_objects moved user_correlations function to WSAS module

--- ws_analysis/analysis_objects.py
+++ ws_analysis/analysis_objects.py
@@ -5,31 +5,31 @@ from .analysis_steps import create_df_daily_steps
 from .analysis_heart_rate import create_df_daily_heart_rate
 
 
-def user_correlations(user_id):
-    print("- in user_correlations ")
-    df, list_of_user_data = create_user_df(user_id=user_id)
-    list_of_arryIndepVarObjects_dict = []
-    if 'HKCategoryTypeIdentifierSleepAnalysis' in list_of_user_data:
-        arryIndepVarObjects_dict = {}
-        # corr_sleep_steps(df)
-        arryIndepVarObjects_dict["name"]= "Step Count"
-        arryIndepVarObjects_dict["depVarName"]= "Sleep Time"
-        correlation_value, obs_count = corr_sleep_steps(df)
-        arryIndepVarObjects_dict["correlationValue"]= correlation_value
-        arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
-        list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
+# def user_correlations(user_id):
+#     print("- in user_correlations ")
+#     df, list_of_user_data = create_user_df(user_id=user_id)
+#     list_of_arryIndepVarObjects_dict = []
+#     if 'HKCategoryTypeIdentifierSleepAnalysis' in list_of_user_data:
+#         arryIndepVarObjects_dict = {}
+#         # corr_sleep_steps(df)
+#         arryIndepVarObjects_dict["name"]= "Step Count"
+#         arryIndepVarObjects_dict["depVarName"]= "Sleep Time"
+#         correlation_value, obs_count = corr_sleep_steps(df)
+#         arryIndepVarObjects_dict["correlationValue"]= correlation_value
+#         arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
+#         list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
 
-    if 'HKQuantityTypeIdentifierHeartRate' in list_of_user_data:
-        arryIndepVarObjects_dict = {}
-        # corr_sleep_heart_rate(df)
-        arryIndepVarObjects_dict["name"]= "Heart Rate Avg"
-        arryIndepVarObjects_dict["depVarName"]= "Sleep Time"
-        correlation_value, obs_count = corr_sleep_heart_rate(df)
-        arryIndepVarObjects_dict["correlationValue"]= correlation_value
-        arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
-        list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
+#     if 'HKQuantityTypeIdentifierHeartRate' in list_of_user_data:
+#         arryIndepVarObjects_dict = {}
+#         # corr_sleep_heart_rate(df)
+#         arryIndepVarObjects_dict["name"]= "Heart Rate Avg"
+#         arryIndepVarObjects_dict["depVarName"]= "Sleep Time"
+#         correlation_value, obs_count = corr_sleep_heart_rate(df)
+#         arryIndepVarObjects_dict["correlationValue"]= correlation_value
+#         arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
+#         list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
     
-    return list_of_arryIndepVarObjects_dict
+#     return list_of_arryIndepVarObjects_dict
 
 def corr_sleep_steps(df):
     print("- in corr_sleep_steps")
