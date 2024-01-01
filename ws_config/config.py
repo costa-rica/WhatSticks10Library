b365ebfbdbfe28ceae0e7aaba9b89d6c9e682d27
Nick Rodriguez <nrodrig1@gmail.com>
2024-01-01 15:58:20 +0100

    refactor: config vars for database folder restructuring

--- ws_config/config.py
+++ ws_config/config.py
@@ -32,19 +32,32 @@ class ConfigBasic():
         self.DB_ROOT = os.environ.get('DB_ROOT')
         self.SQL_URI_WHAT_STICKS_DB = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_WHAT_STICKS')}"
         # self.SQL_URI_BLOG_POSTS = f"sqlite:///{self.DB_ROOT}{os.environ.get('DB_NAME_BLOG_POSTS')}"
-        
+
+        # database helper files
+        self.DATABASE_HELPER_FILES = os.path.join(self.DB_ROOT,"database_helpers")
+        self.APPLE_HEALTH_DIR = os.path.join(self.DATABASE_HELPER_FILES,"apple_health")# <-- store Apple Health compressed
+        self.DATAFRAME_FILES_DIR = os.path.join(self.DATABASE_HELPER_FILES,"dataframe_files")# <-- store pkl files for dashbaord data item
+        self.OURA_SLEEP_RESPONSES = os.path.join(self.DATABASE_HELPER_FILES,"oura_sleep_responses")
+
+        # wsios_helper files
+        self.WS_IOS_HELPER_FILES = os.path.join(self.DB_ROOT,"ws_ios_helpers")
+        self.DASHBOARD_FILES_DIR = os.path.join(self.WS_IOS_HELPER_FILES,"dashboard_table_obj_files")# <-- store pkl files for dashbaord data item
+        self.DATA_SOURCE_FILES_DIR = os.path.join(self.WS_IOS_HELPER_FILES,"data_source_obj_files")# <-- store pkl files for dashbaord data item
+
         #Other Directories in /databases/WhatSticks10
-        self.APPLE_HEALTH_DIR = f"{self.DB_ROOT}apple_health"# <-- store Apple Health compressed
-        # self.DF_FILES_DIR = f"{self.DB_ROOT}df_files"# <-- store pkl files for dashbaord data item
-        self.DASHBOARD_FILES_DIR = f"{self.DB_ROOT}dashboard_files"# <-- store pkl files for dashbaord data item
-        self.DATA_SOURCE_FILES_DIR = f"{self.DB_ROOT}data_source_files"# <-- store pkl files for dashbaord data item
-        self.DATAFRAME_FILES_DIR = f"{self.DB_ROOT}dataframe_files"# <-- store pkl files for dashbaord data item
-        self.DIR_DB_BLOG = f"{self.DB_ROOT}blog"# <-- store blog word documents
-        self.DIR_DB_NEWS = f"{self.DB_ROOT}news"# <-- store blog word documents
+        #self.APPLE_HEALTH_DIR = f"{self.DB_ROOT}apple_health"# <-- store Apple Health compressed
+        # self.DASHBOARD_FILES_DIR = f"{self.DB_ROOT}dashboard_files"# <-- store pkl files for dashbaord data item
+        # self.DATA_SOURCE_FILES_DIR = f"{self.DB_ROOT}data_source_files"# <-- store pkl files for dashbaord data item
+        # self.DATAFRAME_FILES_DIR = f"{self.DB_ROOT}dataframe_files"# <-- store pkl files for dashbaord data item
         self.DIR_DB_AUXIILARY = f"{self.DB_ROOT}auxiliary"# <-- store website files
+        self.DIR_DB_BLOG = os.path.join(self.DIR_DB_AUXIILARY,"blog")# <-- store blog word documents
+        # self.DIR_DB_BLOG = f"{self.DB_ROOT}blog"# <-- store blog word documents
+        self.DIR_DB_NEWS = os.path.join(self.DIR_DB_AUXIILARY,"news")# <-- store blog word documents
+        # self.DIR_DB_NEWS = f"{self.DB_ROOT}news"# <-- store blog word documents
+        
         self.DIR_DB_AUX_IMAGES_PEOPLE = f"{self.DIR_DB_AUXIILARY}/images_people"# <-- store website images of people
         self.DIR_DB_AUX_FILES_UTILITY = f"{self.DIR_DB_AUXIILARY}/files_utility"
-        self.DIR_DB_AUX_OURA_SLEEP_RESPONSES = f"{self.DIR_DB_AUXIILARY}/oura_sleep_responses"
+        # self.DIR_DB_AUX_OURA_SLEEP_RESPONSES = f"{self.DIR_DB_AUXIILARY}/oura_sleep_responses"
         
         # paramters for database/dataframe files
         self.APPLE_HEALTH_QUANTITY_CATEGORY_FILENAME_PREFIX = "AppleHealthQuantityCategory"
