d3e990e96b0355f86ec3d5519e1af8bfc6f509a8
Nick Rodriguez <nrodrig1@gmail.com>
2024-01-02 14:38:51 +0100

    fix: added import to __init__

--- ws_analysis/__init__.py
+++ ws_analysis/__init__.py
@@ -3,4 +3,4 @@ from .analysis_sleep import create_df_daily_sleep
 from .analysis_steps import create_df_daily_steps
 from .analysis_workouts import create_df_daily_workout_duration
 from .analysis_heart_rate import create_df_daily_heart_rate
-from .analysis_objects import corr_sleep_steps, corr_sleep_heart_rate
+from .analysis_objects import corr_sleep_steps, corr_sleep_heart_rate, corr_sleep_workouts
